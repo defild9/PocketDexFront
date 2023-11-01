fdbf213686cad92ff4e801546c1208d320cf0e8f
Yevhenii Biletskyi <biletskijzena@gmail.com>
2023-11-01 23:19:30 +0200

    Init estlint and some libs

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb', 'plugin:react-hooks/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
+  "ignorePatterns": [
+    ".eslintrc.js",
+  ],
+
+};
