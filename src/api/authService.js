baa61e18f20eeb265954d75f8ed951e1ed0d760b
Yevhenii Biletskyi <biletskijzena@gmail.com>
2023-11-06 16:55:11 +0200

    Added service for auth for sing up user

---
+++ src/api/authService.js
@@ -0,0 +1,20 @@
+import apiInstance from './api';
+
+const registerUser = ({ email, username, password }) => {
+  const userData = {
+    username,
+    email,
+    password,
+  };
+
+  return apiInstance.post('/register', userData)
+    .then((response) => {
+      if (response.status === 200) {
+        return response.data;
+      }
+      throw new Error('Error during user registration');
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
