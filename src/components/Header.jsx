5c5af3c9ecf8d28bd8e99f4dd07c654a2a237736
Yevhenii Biletskyi <biletskijzena@gmail.com>
2023-11-16 00:48:49 +0200

    Added for header check if use is authenticated and some links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import {
+  Button, Typography, Toolbar, AppBar,
+} from '@mui/material';
+import { Link } from 'react-router-dom';
+import { checkIfUserIsAuthenticated } from '../utils/authentication';
 
 function Header() {
+  const isAuthenticated = checkIfUserIsAuthenticated();
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6">
-          PocketDex
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+            PocketDex
+          </Link>
         </Typography>
+        {isAuthenticated ? (
+          <Button color="inherit">
+            <Link to="/account" style={{ textDecoration: 'none', color: 'inherit' }}>
+              Account
+            </Link>
+          </Button>
+        ) : (
+          <>
+            <Button color="inherit">
+              <Link to="/login" style={{ textDecoration: 'none', color: 'inherit' }}>
+                Login
+              </Link>
+            </Button>
+            <Button color="inherit">
+              <Link to="/registration" style={{ textDecoration: 'none', color: 'inherit' }}>
+                Sign up
+              </Link>
+            </Button>
+          </>
+        )}
       </Toolbar>
     </AppBar>
   );
